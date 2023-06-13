e2488f28c668707d7d252c50128da42feb2c704e
MANN DESAI <desaimann37@gmail.com>
2023-06-13 14:29:50 +0530

    adding asyncActions.js file...

--- ReduxDemo/index.js
+++ ReduxDemo/index.js
@@ -1,9 +1,16 @@
 
 const redux = require('redux')
+const reduxLogger = require('redux-logger')
+const reduxLogger = require('@reduxjs/toolkit')
 
 
-const createStore = redux.createStore
+const createStore = redux.configureStore
+const combineReducers = redux.combineReducers
+const applyMiddleware = redux.applyMiddleware
+const logger = reduxLogger.createLogger();
+
 const BUY_CAKE = 'BUY_CAKE'
+const BUY_ICE_CREAM = 'BUY_ICECREAM'
 
 
 //Action : 
@@ -13,30 +20,54 @@ function buyCake(){
         info: 'First Redux Action'
     }
 }
+function buyIceCream(){
+    return {
+        type: BUY_ICE_CREAM,
+    }
+}
 
-const initialState = {
+const initialCakeState = {
     numOfCakes: 10,
+}
+const initialIceCreamState = {
+    numOfIcecream: 20,
 }
 
 //Reducer : (prevState , action) => newAtate
-
-const reducer = (state = initialState , action) => {
+const cakeReducer = (state = initialCakeState , action) => {
     switch(action.type){
         case BUY_CAKE: return{
             ...state, 
             numOfCakes: state.numOfCakes - 1
-        };
+        }
         default: return state;
     }
 };
+const iceCreamReducer = (state = initialIceCreamState , action) => {
+    switch(action.type){
+        case BUY_ICE_CREAM: return{
+            ...state,
+            numOfIcecream: state.numOfIcecream - 1
+        }
+        default: return state;
+    }
+};
+const rootReducer = combineReducers({
+    cake: cakeReducer,
+    iceCream: iceCreamReducer
+})
 
-const store = createStore(reducer)
+const store = createStore(rootReducer , applyMiddleware(logger))
 console.log("Initial State " , store.getState())
-const unSubscribe = store.subscribe(() => console.log("Updated State " , store.getState()))
+const unSubscribe = store.subscribe(() => {})
 store.dispatch(buyCake())
 store.dispatch(buyCake())
 store.dispatch(buyCake())
+store.dispatch(buyIceCream())
+store.dispatch(buyIceCream())
 
 unSubscribe()
+
+
 
 
